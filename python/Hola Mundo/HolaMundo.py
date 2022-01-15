aa629bbe80525711a000262e3f2efa36610c3d28
Claudiagreda <97798208+Claudiagreda@users.noreply.github.com>
2022-01-15 12:09:52 -0600

    Actualización

---
+++ python/Hola Mundo/HolaMundo.py
@@ -0,0 +1,12 @@
+# Ejercicio 1. Imprimir Hola Mundo
+
+print("Hola Mundo 1")
+print('Hola Mundo 2')
+print("""Hola Mundo 3""")
+print('''Hola Mundo 4''')
+print("Hola", "Mundo 5") # Concatenación con ,
+print("Hola" + " Mundo 6") # Concatenación con +
+
+print("Suma = ", str(2+3))
+print("Resta = " + str(10-5))
+print(f"Multiplicación = {2*5}") # f (formatear la cadena de texto)
